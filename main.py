56d41b1c6183668189d958d1317ef8b35613d3e9
Sayali Takalkar <80897658+takalkarsayali@users.noreply.github.com>
2026-02-19 11:33:04 +0000

    Complete App

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+# Standard Libraries
 import os
-import streamlit as st
-import pickle
 import time
-
+import pickle
+# Streamlit
+import streamlit as st
+# LangChain LLM
 from langchain_groq import ChatGroq
-
-# from langchain.chains import RetrievalQA
+# LangChain Chain 
+from langchain_classic.chains import RetrievalQA
+# Text Splitter
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-
+# Document Loader
 from langchain_community.document_loaders import UnstructuredURLLoader
+# Vector Store 
 from langchain_community.vectorstores import FAISS
+# Embeddings 
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
+# Environment 
 from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env
 
@@ -28,13 +33,20 @@ process_url_clicked = st.sidebar.button("Process URLs")
 file_path = "faiss_store.pkl"
 main_placeholder = st.empty()
 
+# ---------------- LLM (Groq) ----------------
+llm = ChatGroq(
+    model_name="llama-3.1-8b-instant",
+    temperature=0.3,
+    groq_api_key=os.environ["GROQ_API_KEY"]
+)
+
 if process_url_clicked:
-    # Loading Data
+    # ---------------- Loading Data ----------------
     loader = UnstructuredURLLoader(urls=urls)
     main_placeholder.text("Data Loading... Started ✅")
     data = loader.load()
 
-    # Split data
+    # ---------------- Split data ----------------
     text_splitter = RecursiveCharacterTextSplitter(
         separators=['\n\n','\n','.',','],
         chunk_size = 1000
@@ -42,7 +54,7 @@ if process_url_clicked:
     main_placeholder.text("Text Splitting... Started ✅")
     docs = text_splitter.split_documents(data)
 
-    # create embeddings and save it to FAISS index
+    # ---------------- create embeddings and save it to FAISS index
     embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
@@ -51,6 +63,38 @@ if process_url_clicked:
     main_placeholder.text("Vector Store Created ✅")
     time.sleep(2)
 
-     # Save FAISS index
+     # ---------------- Save FAISS index ----------------
     with open(file_path, "wb") as f:
         pickle.dump(vectorstore, f)
+
+# ---------------- Question Answering ----------------
+query = main_placeholder.text_input("Question: ")
+
+if query:
+    if os.path.exists(file_path):
+        with open(file_path, "rb") as f:
+            vectorstore = pickle.load(f)
+            qa_chain = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=vectorstore.as_retriever(),
+            return_source_documents=True
+            )
+
+            result = qa_chain.invoke({"query": query})
+
+            st.header("Answer")
+            st.subheader(result["result"])
+
+ 
+            # Display sources, if available            
+            st.subheader("Sources")
+
+            unique_sources = set()
+
+            for doc in result["source_documents"]:
+                source = doc.metadata.get("source", "Unknown")
+                unique_sources.add(source)
+
+            for source in unique_sources:
+                st.markdown(f"- [{source}]({source})")
